75401a694ca161867db3972c504ab66df86a7848
Hannes Hergeth <hannes.hergeth@rwth-aachen.de>
2019-06-09 16:21:20 +0000

    added checks to avoid useless work

--- Trakt/ScheduledTasks/SyncLibraryTask.cs
+++ Trakt/ScheduledTasks/SyncLibraryTask.cs
@@ -158,14 +158,17 @@ namespace Trakt.ScheduledTasks
                 var libraryMovie = child as Movie;
                 var userData = _userDataManager.GetUserData(user.Id, child);
 
-                // if movie is not collected, or (export media info setting is enabled and every collected matching movie has different metadata), collect it
-                var collectedMathingMovies = SyncFromTraktTask.FindMatches(libraryMovie, traktCollectedMovies).ToList();
-                if (!collectedMathingMovies.Any()
-                    || (traktUser.ExportMediaInfo
-                        && collectedMathingMovies.All(
-                            collectedMovie => collectedMovie.MetadataIsDifferent(libraryMovie))))
+                if(traktUser.SynchronizeCollections)
                 {
-                    collectedMovies.Add(libraryMovie);
+                    // if movie is not collected, or (export media info setting is enabled and every collected matching movie has different metadata), collect it
+                    var collectedMathingMovies = SyncFromTraktTask.FindMatches(libraryMovie, traktCollectedMovies).ToList();
+                    if (!collectedMathingMovies.Any()
+                        || (traktUser.ExportMediaInfo
+                            && collectedMathingMovies.All(
+                                collectedMovie => collectedMovie.MetadataIsDifferent(libraryMovie))))
+                    {
+                        collectedMovies.Add(libraryMovie);
+                    }
                 }
 
                 var movieWatched = SyncFromTraktTask.FindMatch(libraryMovie, traktWatchedMovies);
@@ -364,13 +367,16 @@ namespace Trakt.ScheduledTasks
                     unplayedEpisodes.Add(episode);
                 }
 
-                var traktCollectedShow = SyncFromTraktTask.FindMatch(episode.Series, traktCollectedShows);
-                if (traktCollectedShow?.seasons == null
-                    || traktCollectedShow.seasons.All(x => x.number != episode.ParentIndexNumber)
-                    || traktCollectedShow.seasons.First(x => x.number == episode.ParentIndexNumber)
-                        .episodes.All(e => e.number != episode.IndexNumber))
+                if(traktUser.SynchronizeCollections)
                 {
-                    collectedEpisodes.Add(episode);
+                    var traktCollectedShow = SyncFromTraktTask.FindMatch(episode.Series, traktCollectedShows);
+                    if (traktCollectedShow?.seasons == null
+                        || traktCollectedShow.seasons.All(x => x.number != episode.ParentIndexNumber)
+                        || traktCollectedShow.seasons.First(x => x.number == episode.ParentIndexNumber)
+                            .episodes.All(e => e.number != episode.IndexNumber))
+                    {
+                        collectedEpisodes.Add(episode);
+                    }
                 }
 
                 decisionProgress.Report(100);
